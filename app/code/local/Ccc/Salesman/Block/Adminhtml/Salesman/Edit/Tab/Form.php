20a31f8d52b9eeef1d10276ee1f38248ba4e45a9
Harsh Parmar <harshparmarhp0@gmail.com>
2023-05-15 11:19:57 +0530

    salesman price grid

---
+++ app/code/local/Ccc/Salesman/Block/Adminhtml/Salesman/Edit/Tab/Form.php
@@ -0,0 +1,71 @@
+<?php
+
+class Ccc_Salesman_Block_Adminhtml_Salesman_Edit_Tab_Form extends Mage_Adminhtml_Block_Widget_Form
+{
+    protected function _prepareForm()
+    {
+        $form = new Varien_Data_Form();
+        $this->setForm($form);
+
+        $fieldset = $form->addFieldset('salesman_form',array('legend'=>Mage::helper('salesman')->__('Salesman information')));
+
+        $fieldset->addField('first_name', 'text', array(
+            'label' => Mage::helper('salesman')->__('First Name'),
+            'required' => true,
+            'name' => 'salesman[first_name]',
+        ));
+
+        $fieldset->addField('last_name', 'text', array(
+            'label' => Mage::helper('salesman')->__('Last Name'),
+            'required' => true,
+            'name' => 'salesman[last_name]',
+        ));
+
+        $fieldset->addField('email', 'text', array(
+            'label' => Mage::helper('salesman')->__('Email'),
+            'required' => true,
+            'name' => 'salesman[email]',
+        ));
+
+        $fieldset->addField('mobile', 'text', array(
+            'label' => Mage::helper('salesman')->__('Mobile'),
+            'required' => true,
+            'name' => 'salesman[mobile]',
+        ));
+
+        $fieldset->addField('gender', 'text', array(
+            'label' => Mage::helper('salesman')->__('Gender'),
+            'required' => true,
+            'name' => 'salesman[gender]',
+        ));
+
+        $fieldset->addField('status', 'text', array(
+            'label' => Mage::helper('salesman')->__('Status'),
+            'required' => true,
+            'name' => 'salesman[status]',
+        ));
+
+        $fieldset->addField('company', 'text', array(
+            'label' => Mage::helper('salesman')->__('Company'),
+            'required' => true,
+            'name' => 'salesman[company]',
+        ));
+
+        if ( Mage::getSingleton('adminhtml/session')->getVendorData() )
+        {
+            $form->setValues(Mage::getSingleton('adminhtml/session')->getVendorData());
+            Mage::getSingleton('adminhtml/session')->setVendorData(null);
+        } 
+        elseif ( Mage::registry('salesman_data') ) {
+            $form->setValues(Mage::registry('salesman_data')->getData());
+        }
+        return parent::_prepareForm();
+    }
+
+}
+
+
+
+
+
+    
