b9a86812fb8534beb6f373dad41e0cbe0bb3d9d6
Harsh Parmar <harshparmarhp0@gmail.com>
2023-05-23 13:20:41 +0530

    vendor module

---
+++ app/code/local/Hp/Vendor/Block/Adminhtml/Vendor/Grid.php
@@ -0,0 +1,76 @@
+<?php
+
+class Hp_Vendor_Block_Adminhtml_Vendor_Grid extends Mage_Adminhtml_Block_Widget_Grid
+{
+
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->setId('vendorAdminhtmlVendorGrid');
+        $this->setDefaultSort('vendor_id');
+        $this->setDefaultDir('ASC');
+    }
+
+   protected function _prepareCollection()
+    {
+        $collection = Mage::getModel('vendor/vendor')->getCollection();
+        /* @var $collection Mage_Cms_Model_Mysql4_Page_Collection */
+        $this->setCollection($collection);
+
+        return parent::_prepareCollection();
+    }
+
+    protected function _prepareColumns()
+    {
+        $baseUrl = $this->getUrl();
+
+        $this->addColumn('first_name', array(
+            'header'    => Mage::helper('vendor')->__('First Name'),
+            'align'     => 'left',
+            'index'     => 'first_name',
+        ));
+
+        $this->addColumn('last_name', array(
+            'header'    => Mage::helper('vendor')->__('Last Name'),
+            'align'     => 'left',
+            'index'     => 'last_name'
+        ));
+
+        $this->addColumn('mobile', array(
+            'header'    => Mage::helper('vendor')->__('mobile'),
+            'align'     => 'left',
+            'index'     => 'mobile'
+        ));
+
+        $this->addColumn('email', array(
+            'header'    => Mage::helper('vendor')->__('Email'),
+            'align'     => 'left',
+            'index'     => 'email'
+        ));
+
+        $this->addColumn('gender', array(
+            'header'    => Mage::helper('vendor')->__('Gender'),
+            'align'     => 'left',
+            'index'     => 'gender',
+            'renderer' => 'Hp_Vendor_Block_Adminhtml_Vendor_Grid_Renderer_Gender',
+
+        ));
+
+        $this->addColumn('status', array(
+            'header'    => Mage::helper('vendor')->__('Status'),
+            'align'     => 'left',
+            'index'     => 'status',
+            'renderer' => 'Hp_Vendor_Block_Adminhtml_Vendor_Grid_Renderer_Status',
+        ));
+
+        return parent::_prepareColumns();
+    }
+
+    
+    public function getRowUrl($row)
+    {
+        return $this->getUrl('*/*/edit', array('vendor_id' => $row->getId()));
+    }
+   
+}
