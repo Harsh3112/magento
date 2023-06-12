f441b86b6cedbb2c6625e8043253c77e4dc8cfaa
Harsh Parmar <harshparmarhp0@gmail.com>
2023-06-12 17:56:39 +0530

    completed 1.0.0

--- app/code/local/Hp/Vendor/Model/Vendor.php
+++ app/code/local/Hp/Vendor/Model/Vendor.php
@@ -6,4 +6,12 @@ class Hp_Vendor_Model_Vendor extends Mage_Core_Model_Abstract
     {
         $this->_init('vendor/vendor');
     }
+
+    public function reset()
+    {
+        $this->setData(array());
+        $this->setOrigData();
+        $this->_attributes = null;
+        return $this;
+    }
 }
